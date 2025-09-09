171e4a5dbe1831de1702f7647266acfc72a82fbf
Kamran <kamranmuazzam@gmail.com>
2025-09-09 10:17:25 +0600

    Updated version to res_ver1.0

---
+++ gg.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Define version variables
+old_version="res_ver1.0"
+version="res_ver1.0"  # <-- bump this when needed
+
+# Replace version strings, but exclude .git folder
+LC_CTYPE=C find . -maxdepth 3 -type f -not -path "./.git/*" \
+  -exec perl -pi -e "s/$old_version/$version/g" {} +
+
+# Git config (only once per system, not needed every run normally)
+git config --global user.name "Kamran"
+git config --global user.email "kamranmuazzam@gmail.com"
+
+# Sync with remote repo
+git pull
+# git pull --rebase origin main
+
+# Commit and push version bump
+git add --all
+git commit -m "Updated version to $version"
+git push -u origin main
+
+# Create GitHub release
+gh release create "v$version" \
+  --title "Version $version" \
+  --notes "Releasing $version 🎉" \
+  --draft=false
